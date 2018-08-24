76dcdad03ebffad02ee0d035b24ef955f53636a9
avillalobosd <lalovide@gmail.com>
2018-08-24 17:12:04 -0500

    make it better

--- server.js
+++ server.js
@@ -21,13 +21,19 @@ app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSB_URL);
+}else {
+  connection= mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "root",
+  password: "",
   database: "burgers_db"
 });
+};
 
 connection.connect(function(err) {
   if (err) {
